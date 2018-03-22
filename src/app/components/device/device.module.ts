df8950872dda77557e5a3de931eab91713adedfa
JustMike112 <justmike112@hotmail.com>
2018-03-22 14:01:32 +0100

    Added Modules and Components
    Modules and Components added to angular: device, issue, notification, participant, reservation, role, room, roomreadings and user.

---
+++ src/app/components/device/device.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DeviceComponent } from './device/device.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [DeviceComponent]
+})
+export class DeviceModule { }
