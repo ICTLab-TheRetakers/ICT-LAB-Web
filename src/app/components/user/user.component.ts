0da185e7e3dce1b6dd4def14f0de879a66bf5816
y0u-s <youssef.lakdime@gmail.com>
2018-06-02 19:04:17 +0200

    Made profile dynamic - Added picture field to user

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,6 +8,12 @@ import { ToastyService, ToastyConfig, ToastOptions, ToastData } from 'ng2-toasty
 })
 export class UserComponent implements OnInit {
 
+
+    firstName = JSON.parse(localStorage.getItem('loggedInUser')).first_name;
+    lastName = JSON.parse(localStorage.getItem('loggedInUser')).last_name;
+    role = JSON.parse(localStorage.getItem('loggedInUser')).role;
+    userId = JSON.parse(localStorage.getItem('loggedInUser')).user_id;
+
   constructor() { }
 
   ngOnInit() {
