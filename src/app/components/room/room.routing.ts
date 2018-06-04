a663a534861f75bd8378d905622ceae682764f62
ICT-LAB Server <bosbosjohan@gmail.com>
2018-06-04 10:01:57 +0200

    Route to add-room comp.

--- src/app/components/room/room.routing.ts
+++ src/app/components/room/room.routing.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RoomComponent } from './room.component';
 import { AuthGuard } from '../../shared/authguard.service';
+import { AddRoomComponent } from './add-room/add-room.component';
 
 const routes: Routes = [
     {
         path: 'rooms',
         children: [
-            { path: '', component: RoomComponent, canActivate: [AuthGuard] }
+            { path: '', component: RoomComponent, canActivate: [AuthGuard] },
+            { path: 'add', component: AddRoomComponent, canActivate: [AuthGuard] }
         ]
     }
 ]; 
