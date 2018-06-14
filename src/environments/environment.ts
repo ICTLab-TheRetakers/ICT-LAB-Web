7bbc6e0ed514d776c73bcafb6761cc8b30d0484e
Johan Bos <bosbosjohan@gmail.com>
2018-06-14 19:42:08 +0200

    Using localhost instead of ip address

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 
 export const environment = {
     production: false,
-    deviceApi: 'http://145.24.222.238/api/devices/',
-    issueApi: 'http://145.24.222.238/api/issues/',
-    notificationsApi: 'http://145.24.222.238/api/notifications/',
-    readingApi: 'http://145.24.222.238/api/readings/',
-    reservationApi: 'http://145.24.222.238/api/reservations/',
-    roleApi: 'http://145.24.222.238/api/roles/',
-    roomApi: 'http://145.24.222.238/api/rooms/',
-    userApi: 'http://145.24.222.238/api/users/',
+    deviceApi: 'http://localhost/api/devices/',
+    issueApi: 'http://localhost/api/issues/',
+    notificationsApi: 'http://localhost/api/notifications/',
+    readingApi: 'http://localhost/api/readings/',
+    reservationApi: 'http://localhost/api/reservations/',
+    roleApi: 'http://localhost/api/roles/',
+    roomApi: 'http://localhost/api/rooms/',
+    userApi: 'http://localhost/api/users/',
     hours: ['8:30-9:20', '9:20-10:10', '10:30-11:20', '11:20-12:10', '12:10-13:00', '13:00-13:50', '13:50-14:40', '15:00-15:50',
         '15:50-16:40', '17:00-17:50', '17:50-18:40', '18:40-19:30', '19:30-20:20', '20:20-21:10', '21:10-22:00'],
 };
