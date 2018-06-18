8403ec08b580845f38a55c86cb882d33c5bb2db2
y0u-s <youssef.lakdime@gmail.com>
2018-06-18 13:35:32 +0200

    5 reservations max allowed

--- src/app/components/device/add-device/add-device.component.ts
+++ src/app/components/device/add-device/add-device.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
+import {DeviceService} from "../../../shared/services/device.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import Room from "../../../shared/models/room.model";
+import Device from "../../../shared/models/device.model";
 
 @Component({
   selector: 'app-add-device',
@@ -8,17 +12,43 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class AddDeviceComponent implements OnInit {
 
+    device: Device = null;
+    selectedRoom: Room = null;
+    deviceid: number;
     room: string;
+    hasDevice: boolean;
 
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute, private _deviceService: DeviceService) {
+      this.room = this.route.snapshot.paramMap.get('room');
+      this._deviceService.getByRoom(this.room).subscribe(
+          (response) => {
 
-    /*  this.route.queryParams.subscribe(params => {
-          this.room = params['room'];
-      });*/
+              this.deviceid = response[0].device_id;
+          },
+          (error: HttpErrorResponse) => { throw error; }
+      );
   }
 
   ngOnInit() {
-      this.room = this.route.snapshot.paramMap.get('room');
+
   }
+
+    getDeviceByRoom() {
+        this._deviceService.getByRoom(this.selectedRoom.room_code).subscribe(
+            (response) => {
+                this.device = response[0];
+                //console.log('has device: ', response[0])
+
+                if (!response[0]) {
+
+                    this.hasDevice = false;
+                    console.log('has device: ', this.hasDevice);
+                }
+
+
+            },
+            (error: HttpErrorResponse) => { throw error; }
+        );
+    }
 
 }
--- src/app/components/reservation/add/add-reservation.component.ts
+++ src/app/components/reservation/add/add-reservation.component.ts
@@ -29,6 +29,7 @@ export class AddReservationComponent implements OnInit {
     minDate: string;
     maxDate: string;
     date;
+    reservationLimit: boolean;
 
     constructor(private _reservationService: ReservationService, private router: Router, private toastyService: ToastyService) {
         this.toastOptions = {
@@ -99,10 +100,20 @@ export class AddReservationComponent implements OnInit {
         reservation.date = this.minDate;
 
         this.reservations.push(reservation);
+        this.checkReservationLimit();
+
     }
 
+    checkReservationLimit() {
+        if (this.reservations.length >= 5) {
+            this.reservationLimit = true;
+        } else {
+            this.reservationLimit = false;
+        }
+    }
     removeRow(index: number) {
         this.reservations.splice(index, 1);
+       this.checkReservationLimit();
     }
 
     convertDatetime(reservation: Reservation): Reservation {
