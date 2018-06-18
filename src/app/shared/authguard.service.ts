0a83250d6c624c1252290b0b16db4414f6fef571
Johan Bos <bosbosjohan@gmail.com>
2018-06-18 16:47:48 +0200

    authguard fix

--- src/app/shared/authguard.service.ts
+++ src/app/shared/authguard.service.ts
@@ -12,20 +12,11 @@ export class AuthGuard implements CanActivate {
             let user = JSON.parse(sessionStorage.getItem('loggedInUser')) as User;
             let roles = route.data["roles"] as Array<number>;
 
-            if (roles != null && roles.length > 0) {
-                if (roles.length == 1) {
-                    if (user.role_id == roles[0]) {
-                        return true;
-                    }
-                } else {
-                    if (roles.indexOf(user.role_id) != -1) {
-                        return true;
-                    }
-                }
+            if (roles.indexOf(user.role_id) != -1) {
+                return true;
             }
 
-            // logged in so return true
-            return true;
+            return false;
         }
 
         // not logged in so redirect to login page with the return url
