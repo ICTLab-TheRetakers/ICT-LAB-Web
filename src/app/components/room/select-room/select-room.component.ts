2793de6f8dacd6c6ab640958672ee07fc368185f
Johan Bos <bosbosjohan@gmail.com>
2018-07-01 17:24:25 +0200

    Check if fixed

--- src/app/components/room/select-room/select-room.component.ts
+++ src/app/components/room/select-room/select-room.component.ts
@@ -93,9 +93,9 @@ export class SelectRoomComponent implements OnInit {
         }
 
         this.startWeek = moment(today).week();
-        if (dayOfWeek == 0 || dayOfWeek == 6 || (dayOfWeek == 5 && time >= 22)) {
-            this.startWeek = this.startWeek + 1;
-        }
+        //if (dayOfWeek == 0 || dayOfWeek == 6 || (dayOfWeek == 5 && time >= 22)) {
+        //    this.startWeek = this.startWeek + 1;
+        //}
 
         if (quarter == 3 && week <= 27) {
             this.quarter = '4';
