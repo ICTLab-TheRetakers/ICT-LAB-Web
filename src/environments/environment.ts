f8158de82851e5a7ce7ec30d57b27c099eaec272
JustMike112 <justmike112@hotmail.com>
2018-07-05 16:41:58 +0200

    Local Environment
    Now using my local environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 
 export const environment = {
     production: false,
-    deviceApi: 'http://145.24.222.238/api/devices/',
-    issueApi: 'http://145.24.222.238/api/issues/',
-    notificationsApi: 'http://145.24.222.238/api/notifications/',
-    readingApi: 'http://145.24.222.238/api/readings/',
-    reservationApi: 'http://145.24.222.238/api/reservations/',
-    roleApi: 'http://145.24.222.238/api/roles/',
-    roomApi: 'http://145.24.222.238/api/rooms/',
-    userApi: 'http://145.24.222.238/api/users/',
+    deviceApi: '/api/devices/',
+    issueApi: '/api/issues/',
+    notificationsApi: '/api/notifications/',
+    readingApi: '/api/readings/',
+    reservationApi: '/api/reservations/',
+    roleApi: '/api/roles/',
+    roomApi: '/api/rooms/',
+    userApi: '/api/users/',
     hours: ['8:30-9:20', '9:20-10:10', '10:30-11:20', '11:20-12:10', '12:10-13:00', '13:00-13:50', '13:50-14:40', '15:00-15:50',
         '15:50-16:40', '17:00-17:50', '17:50-18:40', '18:40-19:30', '19:30-20:20', '20:20-21:10', '21:10-22:00'],
 };
